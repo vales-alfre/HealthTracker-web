7e230b65428d9db6945be089d7ff8b00b4708042
vales-alfre <97996152+vales-alfre@users.noreply.github.com>
2024-01-16 23:29:19 -0500

    version con tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './login.jsx'
 import './index.css'
+import Iniciosecion from './login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Iniciosecion />
   </React.StrictMode>,
 )
