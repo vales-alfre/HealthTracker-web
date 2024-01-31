70e0f4d13fa7e28f9101ccf47b5ff613f38b81ae
vales-alfre <97996152+vales-alfre@users.noreply.github.com>
2024-01-30 23:40:15 -0500

    admin y cuidador

--- src/Routers/AppRouters.jsx
+++ src/Routers/AppRouters.jsx
@@ -4,6 +4,13 @@ import Iniciosecion from '../Pages/login'
 import Recover from '../Pages/Recover'
 import Home from '../Pages/Home'
 import Perfil from '../Pages/profile'
+<<<<<<< Updated upstream
+=======
+import PatientRegistration from '../Paciente/Register'
+import ListaPacientes from '../Paciente/list'
+import ListaCuidador from '../Cuidador/list_curer'
+import ListaAdmin from '../Admin/list_admin'
+>>>>>>> Stashed changes
 
 const appRouters = () => {
   return (
@@ -12,6 +19,14 @@ const appRouters = () => {
         <Route path='/Home' element={<Home/>}/>
         <Route path='/Recuperar-contraseña' element={<Recover/>}/> 
         <Route path='/Perfil' element={<Perfil/>}/> 
+<<<<<<< Updated upstream
+=======
+        <Route path='/Registrar_paciente' element={<PatientRegistration/>}/>
+        <Route path='/Lista_paciente' element={<ListaPacientes/>}/>
+        <Route path='/Lista_cuidador' element={<ListaCuidador/>}/>
+        <Route path='/Lista_Admin' element={<ListaAdmin/>}/>
+        
+>>>>>>> Stashed changes
     </Routes>
   )
 }
