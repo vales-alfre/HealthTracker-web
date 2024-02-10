7db45d27661ef7a3d77721cd3704f88b483502b2
vales-alfre <97996152+vales-alfre@users.noreply.github.com>
2024-02-09 22:53:50 -0500

    dale barcaaaaa

--- src/Routers/AppRouters.jsx
+++ src/Routers/AppRouters.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { ProtectedRoute } from './ProtectedRoute'; // Asegúrate de importar el componente correctamente
 import Iniciosecion from '../Pages/login'
 import Recover from '../Pages/Recover'
 import Home from '../Pages/Home'
@@ -15,35 +14,35 @@ function AppRouters () {
         <Route path='/' element={<Iniciosecion/>}/>
         <Route path='/Home' element={
             // Envuelve Home dentro de ProtectedRoute sin prop de roles
-            <ProtectedRoute>
+           
                 <Home/>
-            </ProtectedRoute>
+            
         }/>
         <Route path='/Recuperar-contraseña' element={<Recover/>}/> 
         <Route path='/Perfil' element={
             // ProtectedRoute sin prop de roles para Perfil
-            <ProtectedRoute>
+           
                 <Perfil/>
-            </ProtectedRoute>
+            
         }/>
         
         <Route path='/Lista_paciente' element={
             // ProtectedRoute sin prop de roles para ListaPacientes
-            <ProtectedRoute>
+           
                 <ListaPacientes/>
-            </ProtectedRoute>
+           
         }/>
         <Route path='/Lista_Admin' element={
             // ProtectedRoute sin prop de roles para ListaAdmin
-            <ProtectedRoute>
+           
                 <ListaAdmin/>
-            </ProtectedRoute>
+           
         }/>
         <Route path='/Lista_cuidador' element={
             // ProtectedRoute sin prop de roles para Curer
-            <ProtectedRoute>
+           
                 <Curer/>
-            </ProtectedRoute>
+         
         }/>
     </Routes>
   )
