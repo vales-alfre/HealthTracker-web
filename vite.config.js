38200176ff8c92f7b62d376278c8c387eca0b9df
vales-alfre <97996152+vales-alfre@users.noreply.github.com>
2024-02-09 21:21:50 -0500

    api

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Todas las solicitudes que comiencen con '/api' serán redirigidas
+      '/api': {
+        target: 'https://carinosaapi.onrender.com', // La URL base de tu API
+        changeOrigin: true, // Necesario para evitar problemas de CORS
+        rewrite: (path) => path.replace(/^\/api/, '') // Opcional: reescribe las URLs de las solicitudes
+      },
+    },
+  },
+});
